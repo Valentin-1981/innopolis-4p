464682da3ae5393248c9bcf358044bcbc7384d8c
Valentine Chibinyov <chibinyov@gmail.com>
2022-11-09 20:25:54 +0300

    homework_10 v2

--- homework_10/script.js
+++ homework_10/script.js
@@ -1,44 +1,55 @@
-// var dataObj = null;
-
-const loadPosts = async () => {
-    const response = await fetch("https://reqres.in/api/users");
-
-    const posts = await response.json();
-
-    return posts;
-};
-
-let load = loadPosts();
-
-console.log(load);
-
-// let dataObj = await fetch("https://reqres.in/api/users");
-// let innerData = await dataObj.json();
-
-const Data = fetch("https://reqres.in/api/users")
+fetch("https://reqres.in/api/users")
     .then((response) => {
         return response.json();
     })
     .then((data) => {
-        // dataObj = data;
         console.log(data);
-        // console.log(Object.entries(dataObj));
-        return data;
+
+        console.log("-----------");
+        console.log("Пункт No1:");
+        console.log("-----------");
+
+        for (let token of data.data) {
+            console.log(token);
+        }
+
+        let Data = data.data;
+
+        console.log("-----------");
+        console.log("Пункт No2:");
+        console.log("-----------");
+        for (let lastName of Data) {
+            console.log(lastName.last_name);
+        }
+
+        console.log("-----------");
+        console.log("Пункт No3:");
+        console.log("-----------");
+        for (let index of Data) {
+            if (index.last_name[0] === "W") {
+                console.log(index);
+            }
+        }
+
+        const result = Data.reduce((accumulator, item, index, array) => {
+            accumulator += item.first_name + " " + item.last_name + ", ";
+            return accumulator;
+        }, "");
+
+        console.log("-----------");
+        console.log("Пункт No4:");
+        console.log("-----------");
+
+        console.log(
+            "Наша база данных содержит данные следующих пользователей: ",
+            result
+        );
+
+        console.log("-----------");
+        console.log("Пункт No5:");
+        console.log("-----------");
+
+        Object.entries(Data[0]).forEach(([key, value]) => {
+            console.log("Ключ: ", key);
+        });
     });
-
-let Load = Data.then((token) => {
-    return token;
-});
-
-console.log(Load);
-
-// let innerData_2 = innerData["data"];
-
-// console.log(innerData_2);
-
-// let innerData_3 = Object.entries(innerData_2);
-// console.log(innerData_2);
-
-// for (let [key, value] of innerData_3) {
-//     console.log(value);
-// }
